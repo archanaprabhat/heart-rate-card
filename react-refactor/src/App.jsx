e264fa680ad235e3026ec6e05bea8745bbf48682
archanaprabhat <archanaprabhath2002@gmail.com>
2026-05-07 14:24:56 +0530

    Add Card component and implement card data mapping in App

---
+++ react-refactor/src/App.jsx
@@ -0,0 +1,31 @@
+import Card from "./components/Card"
+
+const cardsData = [{
+  id:1,
+  theme:'red',
+  value:86,
+  averages:{
+    weekly:96,
+    monthly:77
+  }
+},
+{
+  id:2,
+  theme:'green',
+  value:61,
+  averages:{
+    weekly:86,
+    monthly:62
+  }
+}
+]
+
+export default function App(){
+  return(
+    <>
+   {cardsData.map(item => (
+    <Card key={item.id} data={item}/>
+   ))}
+    </>
+  )
+}
---
+++ react-refactor/src/components/Card.jsx
@@ -0,0 +1,4 @@
+export default function Card({data}) {
+  console.log(data.value)         
+  return null
+}
